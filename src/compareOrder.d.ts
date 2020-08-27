da5a3559fd7614d986e8bf21829e53d2bffc9f5e
Robb Lovell <robb@codezero.io>
2020-08-26 21:14:51 -0700

    v1.0.2

--- src/compareOrder.d.ts
+++ src/compareOrder.d.ts
@@ -1,1 +1,1 @@
-export declare const compareOrder: (a: any, b: any) => 0 | 1 | -1;
+export declare const compareOrder: (a: any, b: any) => 1 | -1 | 0;
--- src/compareOrder.js
+++ src/compareOrder.js
@@ -4,7 +4,6 @@ exports.compareOrder = void 0;
 exports.compareOrder = (a, b) => {
     const nameA = a.toUpperCase();
     const nameB = b.toUpperCase();
-    return 0;
     if (nameA < nameB) {
         return -1;
     }
